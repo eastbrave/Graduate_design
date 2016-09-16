7057cbef79c1dccdd36558a3717d3ad365945c56
lonelywo <850411893@qq.com>
2016-09-17 00:16:32 +0800

    9.16 yan

---
+++ daoway/src/main/java/com/android/graduate/daoway/d_order/OrderActivity.java
@@ -0,0 +1,15 @@
+package com.android.graduate.daoway.d_order;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.graduate.daoway.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
